ea9e2e23c2564c51e70c71d7d9be66c3e75bec29
DanielMejia95 <luisdanielmejia@outlook.com>
2017-10-04 15:26:23 -0500

    Api key security add

--- BikeMeServer/wsgi.py
+++ BikeMeServer/wsgi.py
@@ -1,16 +1,17 @@
-"""
-WSGI config for RestAPI project.
+import os, sys
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+#path a donde esta el manage.py de nuestro proyecto Django
+sys.path.append("/home/ubuntu/BikeMe/BikeMeServer/")
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
-"""
+#referencia (en python) desde el path anterior al fichero settings.py
+#Importante hacerlo así, si hay varias instancias coriendo (en lugar de setdefault)
+os.environ["DJANGO_SETTINGS_MODULE"] = "BikeMeServer.settings"
+#os.environ.setdefault(“DJANGO_SETTINGS_MODULE”, “proyectodjango.settings”)
 
-import os
+#prevenimos UnicodeEncodeError
+os.environ.setdefault("LANG", "en_US.UTF-8")
+os.environ.setdefault("LC_ALL", "en_US.UTF-8")
 
+#obtenemos la aplicación
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BikeMeServer.settings")
-
 application = get_wsgi_application()
